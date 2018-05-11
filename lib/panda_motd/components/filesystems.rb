99ba15d4c4c330f1141f04f1868baa8de559eefd
Taylor Thurlow <taylorthurlow8@gmail.com>
2018-05-11 16:43:17 -0700

    Filesystems component tweaks

--- lib/panda_motd/components/filesystems.rb
+++ lib/panda_motd/components/filesystems.rb
@@ -17,63 +17,67 @@ class Filesystems
   end
 
   def to_s
-    name_col_size = @results.select { |r| r.is_a? Hash }.map { |r| r[:pretty_name].length }.max
-    name_col_size = 0 if name_col_size.nil?
+    if @results.any?
+      name_col_size = @results.select { |r| r.is_a? Hash }.map { |r| r[:pretty_name].length }.max
+      name_col_size = 0 if name_col_size.nil?
 
-    name_col_size_with_padding = (name_col_size + 6) > 13 ? (name_col_size + 6) : 13
+      name_col_size_with_padding = (name_col_size + 6) > 13 ? (name_col_size + 6) : 13
 
-    result = 'Filesystems'.ljust(name_col_size_with_padding, ' ')
-    result += "Size  Used  Free  Use%\n"
+      result = 'Filesystems'.ljust(name_col_size_with_padding, ' ')
+      result += "Size  Used  Free  Use%\n"
 
-    @results.each do |filesystem|
-      # handle filesystem not found
-      if filesystem.is_a? String
-        result += "  #{filesystem}\n"
-        next
+      @results.each do |filesystem|
+        # handle filesystem not found
+        if filesystem.is_a? String
+          result += "  #{filesystem}\n"
+          next
+        end
+
+        result += "  #{filesystem[:pretty_name]}".ljust(name_col_size_with_padding, ' ')
+
+        [:size, :used, :avail].each do |metric|
+          units = if filesystem[metric] > 10**12
+                    'terabytes'
+                  elsif filesystem[metric] > 10**9
+                    'gigabytes'
+                  elsif filesystem[metric] > 10**6
+                    'megabytes'
+                  elsif filesystem[metric] > 10**3
+                    'kilobytes'
+                  else
+                    'bytes'
+                  end
+
+          value = Unit.new("#{filesystem[metric]} bytes").convert_to(units)
+
+          # we have 4 characters of space to include the number, a potential decimal point, and
+          # the unit character at the end. if the whole number component is 3+ digits long then
+          # we omit the decimal point and just display the whole number component. if the whole
+          # number component is 2 digits long, we can't afford to use a decimal point, so we still
+          # only display the whole number component. if the whole number component is 1 digit long,
+          # we use the single whole number component digit, a decimal point, a single fractional
+          # digit, and the unit character.
+          whole_number_length = value.scalar.floor.to_s.length
+          round_amount = whole_number_length > 1 ? 0 : 1
+          formatted_value = value.scalar.round(round_amount).to_s + units[0].upcase
+          result += formatted_value.rjust(4, ' ') + '  '
+        end
+
+        percentage_used = ((filesystem[:used].to_f / filesystem[:size].to_f) * 100).round
+        result += (percentage_used.to_s.rjust(3, ' ') + '%').send(percentage_color(percentage_used))
+
+        result += "\n"
+
+        total_ticks = name_col_size_with_padding + 18
+        used_ticks = (total_ticks * (percentage_used.to_f / 100)).round
+
+        result += "  [#{('=' * used_ticks).send(percentage_color(percentage_used))}#{('=' * (total_ticks - used_ticks)).light_black}]\n"
       end
 
-      result += "  #{filesystem[:pretty_name]}".ljust(name_col_size_with_padding, ' ')
-
-      [:size, :used, :avail].each do |metric|
-        units = if filesystem[metric] > 10**12
-                  'terabytes'
-                elsif filesystem[metric] > 10**9
-                  'gigabytes'
-                elsif filesystem[metric] > 10**6
-                  'megabytes'
-                elsif filesystem[metric] > 10**3
-                  'kilobytes'
-                else
-                  'bytes'
-                end
-
-        value = Unit.new("#{filesystem[metric]} bytes").convert_to(units)
-
-        # we have 4 characters of space to include the number, a potential decimal point, and
-        # the unit character at the end. if the whole number component is 3+ digits long then
-        # we omit the decimal point and just display the whole number component. if the whole
-        # number component is 2 digits long, we can't afford to use a decimal point, so we still
-        # only display the whole number component. if the whole number component is 1 digit long,
-        # we use the single whole number component digit, a decimal point, a single fractional
-        # digit, and the unit character.
-        whole_number_length = value.scalar.floor.to_s.length
-        round_amount = whole_number_length > 1 ? 0 : 1
-        formatted_value = value.scalar.round(round_amount).to_s + units[0].upcase
-        result += formatted_value.rjust(4, ' ') + '  '
-      end
-
-      percentage_used = ((filesystem[:used].to_f / filesystem[:size].to_f) * 100).round
-      result += (percentage_used.to_s.rjust(3, ' ') + '%').send(percentage_color(percentage_used))
-
-      result += "\n"
-
-      total_ticks = name_col_size_with_padding + 18
-      used_ticks = (total_ticks * (percentage_used.to_f / 100)).round
-
-      result += "  [#{('=' * used_ticks).send(percentage_color(percentage_used))}#{('=' * (total_ticks - used_ticks)).light_black}]\n"
+      return result
+    else
+      return "Filesystems:\n  No matching filesystems found."
     end
-
-    return result
   end
 
   private
@@ -94,9 +98,9 @@ class Filesystems
     header = command_result[0].split
     entries = command_result[1..command_result.count]
 
-    name_index = find_header_id_by_text(header, 'filesystem')
-    size_index = find_header_id_by_text(header, 'blocks')
-    used_index = find_header_id_by_text(header, 'used')
+    name_index  = find_header_id_by_text(header, 'filesystem')
+    size_index  = find_header_id_by_text(header, 'blocks')
+    used_index  = find_header_id_by_text(header, 'used')
     avail_index = find_header_id_by_text(header, 'avail')
 
     results = filesystems.map do |filesystem, name|
