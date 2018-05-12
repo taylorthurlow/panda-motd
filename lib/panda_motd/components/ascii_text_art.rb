a49c0f44ec215abed3fea85c62f402fe355b9a33
Taylor Thurlow <taylorthurlow8@gmail.com>
2018-05-11 17:49:32 -0700

    Add ascii text art spec, add error handling

--- lib/panda_motd/components/ascii_text_art.rb
+++ lib/panda_motd/components/ascii_text_art.rb
@@ -2,7 +2,7 @@ require 'artii'
 require 'colorize'
 
 class ASCIITextArt
-  attr_reader :name, :errors
+  attr_reader :name, :errors, :results
 
   def initialize(motd)
     @name = 'ascii_text_art'
@@ -13,10 +13,17 @@ class ASCIITextArt
 
   def process
     @text = `hostname`
-    @art = Artii::Base.new font: @config['font']
+
+    begin
+      @art = Artii::Base.new font: @config['font']
+      @results = @art.asciify(@text)
+      @results = @results.send(@config['color'].to_sym) if @config['color']
+    rescue Errno::EISDIR # Artii doesn't handle invalid font names very well
+      @errors << ComponentError.new(self, 'Invalid font name')
+    end
   end
 
   def to_s
-    return @art.asciify(@text).send(@config['color'].to_sym)
+    return @results
   end
 end
