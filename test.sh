a4c35811437d51a2cfaf7248a577690e5e90c942
Taylor Thurlow <taylorthurlow8@gmail.com>
2018-05-12 18:50:49 -0700

    Use spaces instead of tabs in test.sh

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ rm *.gem
 gem build panda-motd.gemspec
 scp panda-motd*.gem taylor@taylorjthurlow.com:~/temp/pandamotd.gem
 ssh -t taylor@taylorjthurlow.com "cd ~/temp && \
-																	sudo gem uninstall -x panda-motd && \
-																	sudo gem install ./*.gem && \
-																	sudo panda-motd"
+                                  sudo gem uninstall -x panda-motd && \
+                                  sudo gem install ./*.gem && \
+                                  sudo panda-motd"
