50e6c763f1d9a09e7e791baa7d9b26bf4c93aea5
Taylor Thurlow <taylorthurlow8@gmail.com>
2018-10-28 12:28:04 -0700

    Update test script for new domain name

--- test.sh
+++ test.sh
@@ -2,10 +2,10 @@
 
 rm *.gem
 gem build panda-motd.gemspec
-scp panda-motd*.gem taylor@taylorjthurlow.com:~/temp/pandamotd.gem
-ssh -t taylor@taylorjthurlow.com "cd ~/temp && \
-                                  sudo gem uninstall -x panda-motd && \
-                                  sudo gem install ./*.gem && \
-                                  echo '---------------------' && \
-                                  sudo panda-motd /home/taylor/.config/panda-motd.yaml && \
-                                  echo '---------------------'"
+scp panda-motd*.gem taylor@home.thurlow.io:~/temp/pandamotd.gem
+ssh -t taylor@home.thurlow.io "cd ~/temp && \
+                               sudo gem uninstall -x panda-motd && \
+                               sudo gem install ./*.gem && \
+                               echo '---------------------' && \
+                               sudo panda-motd /home/taylor/.config/panda-motd.yaml && \
+                               echo '---------------------'"
