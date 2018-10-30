de9751a9efcde360fbe09172fb3ac4a84c3abce7
Taylor Thurlow <taylorthurlow8@gmail.com>
2018-10-30 14:37:59 -0700

    Rewrite ascii_text_art component with factory_bot

--- spec/components/ascii_text_art_spec.rb
+++ spec/components/ascii_text_art_spec.rb
@@ -2,33 +2,34 @@ require 'spec_helper'
 require 'colorize'
 
 describe ASCIITextArt do
-  before do
-    described_class_instance.process
-  end
-
   context 'with normal config' do
-    let(:described_class_instance) {
-      instance_with_configuration(described_class, 'enabled' => true, 'font' => 'slant', 'color' => 'red', 'command' => 'hostname')
-    }
+    subject(:component) { create(:ascii_text_art) }
 
     it 'prints the properly colored art' do
-      expect(described_class_instance.results).to start_with "\e[0;31;49m" # escape codes for red text
-      expect(described_class_instance.results).to end_with "\e[0m"
+      stub_system_call(component, 'helloworld')
+      component.process
+
+      expect(component.results).to start_with "\e[0;31;49m" # escape codes for red text
+      expect(component.results).to end_with "\e[0m"
     end
 
     it 'prints the art' do
-      expect(described_class_instance.to_s).not_to be_nil
+      stub_system_call(component, 'helloworld')
+      component.process
+
+      expect(component.to_s).not_to be_nil
     end
   end
 
   context 'with config containing an invalid font name' do
-    let(:described_class_instance) {
-      instance_with_configuration(described_class, 'enabled' => true, 'font' => 'badfontname', 'color' => 'red', 'command' => 'hostname')
-    }
+    subject(:component) { create(:ascii_text_art, settings: { 'font' => 'badfontname' }) }
 
     it 'adds an error to the component' do
-      expect(described_class_instance.errors.count).to eq 1
-      expect(described_class_instance.errors.first.message).to eq 'Invalid font name'
+      stub_system_call(component, 'helloworld')
+      component.process
+
+      expect(component.errors.count).to eq 1
+      expect(component.errors.first.message).to eq 'Invalid font name'
     end
   end
 end
