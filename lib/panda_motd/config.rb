fa703386f90193d7ded5977856d7c4e5c3c3eacd
Taylor Thurlow <taylorthurlow8@gmail.com>
2018-11-02 12:00:09 -0700

    Refactor config

--- lib/panda_motd/config.rb
+++ lib/panda_motd/config.rb
@@ -5,7 +5,7 @@ class Config
   attr_reader :file_path
 
   def initialize(file_path = nil)
-    @file_path = file_path.nil? ? File.join(Dir.home, '.config', 'panda-motd.yaml') : file_path
+    @file_path = file_path || File.join(Dir.home, '.config', 'panda-motd.yaml')
     unless File.exist?(@file_path)
       create_config(@file_path)
       puts "panda-motd created a default config file at: #{@file_path}"
@@ -14,18 +14,18 @@ class Config
   end
 
   def components_enabled
-    # first iterate through the config hash and grab all names of enabled components
-    enabled_list = @config['components'].map { |component, setting| component if setting['enabled'] }.compact
+    # iterate config hash and grab names of enabled components
+    enabled = @config['components'].map { |c, s| c if s['enabled'] }.compact
     # get the class constant
-    return enabled_list.map { |e| Config.component_classes[e.to_sym] }
+    enabled.map { |e| Config.component_classes[e.to_sym] }
   end
 
   def component_config(component_name)
-    return @config['components'][component_name.to_s]
+    @config['components'][component_name.to_s]
   end
 
   def self.component_classes
-    return {
+    {
       ascii_text_art: ASCIITextArt,
       service_status: ServiceStatus,
       uptime: Uptime,
@@ -38,7 +38,9 @@ class Config
   private
 
   def create_config(file_path)
-    default_config_path = File.join(File.dirname(__dir__), 'panda_motd', 'default_config.yaml')
+    default_config_path = File.join(
+      File.dirname(__dir__), 'panda_motd', 'default_config.yaml'
+    )
     FileUtils.cp(default_config_path, file_path)
   end
 
