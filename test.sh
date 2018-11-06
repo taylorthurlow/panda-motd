f5b712870fa01872a7c392a70fad499f79209a2c
Taylor Thurlow <taylorthurlow8@gmail.com>
2018-11-05 16:29:21 -0800

    Test script exits on error, properly rm -f

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-rm *.gem
+set -e
+
+rm -f *.gem
 gem build panda-motd.gemspec
 scp panda-motd*.gem taylor@home.thurlow.io:~/temp/pandamotd.gem
 ssh -t taylor@home.thurlow.io "cd ~/temp && \
