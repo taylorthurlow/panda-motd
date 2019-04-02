997f79dcff524d4b3c96da67328942f551826f15
Taylor Thurlow <taylorthurlow8@gmail.com>
2019-04-02 14:47:32 -0700

    Get version from release to use in rubygems release

--- action-release/entrypoint.sh
+++ action-release/entrypoint.sh
@@ -1,9 +1,16 @@
-#!/bin/sh -l
+#!/bin/bash -l
 
 set -eu
 
+apt update -qq
+apt install -y jq
+
+VERSION_FROM_TAG=$(cat $GITHUB_EVENT_PATH | jq .release.tag_name)
+VERSION_FROM_TAG=$(echo $VERSION_FROM_TAG | sed 's/^"//' | sed 's/"$//')
+VERSION_FROM_TAG=$(echo $VERSION_FROM_TAG | sed 's/^v//')
+
 bundle install
 
-bundle exec gem bump -v $1 --pretend
+bundle exec gem bump -v $VERSION_FROM_TAG --pretend
 bundle exec gem tag --pretend
 bundle exec gem release --pretend
