4efae2d08146cdec7468ff6d6147620921181c95
Taylor Thurlow <taylorthurlow8@gmail.com>
2019-04-01 23:06:30 -0700

    Suppress curl output when downloading CC test reporter

--- action-test/entrypoint.sh
+++ action-test/entrypoint.sh
@@ -7,7 +7,7 @@ apt install -y curl
 
 bundle install
 
-curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+curl -sL https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
 chmod +x ./cc-test-reporter
 ./cc-test-reporter before-build
 
