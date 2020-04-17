74bffc6591c2368db8834f2272bca3453861cc7e
Taylor Thurlow <taylorthurlow@me.com>
2020-04-17 01:03:26 -0700

    Add type signatures to LastLogin, fix errors

--- lib/panda_motd/components/last_login.rb
+++ lib/panda_motd/components/last_login.rb
@@ -4,16 +4,20 @@
 require "date"
 
 class LastLogin < Component
+  sig { params(motd: MOTD).void }
+
   def initialize(motd)
     super(motd, "last_login")
   end
 
+  sig { void }
   # @see Component#process
   def process
     @users = @config["users"]
     @results = parse_last_logins
   end
 
+  sig { returns(String) }
   # @see Component#to_s
   def to_s
     <<~HEREDOC
@@ -24,53 +28,51 @@ class LastLogin < Component
 
   private
 
+  sig do
+    params(
+      user: String, # the username to generate a string for
+      logins: T::Array[T::Hash[Symbol, T.untyped]], # username keys with values containing login data
+    ).returns(String) # the printable string for a single user
+  end
   # Takes the list of processed results and generates a complete printable
   # component string.
-  #
-  # @param user [String] the username to generate a string for
-  # @param logins [Array<Hash>] an array of hashes with username keys and hash
-  #   values containing the login data (see {#hashify_login})
-  #
-  # @return [String] the printable string for a single user
   def parse_result(user, logins)
     logins_part = if logins.empty?
-                    "    no logins found for user."
-                  else
-                    longest_size = logins.map { |l| l[:location].length }.max
-                    logins.map { |l| parse_login(l, longest_size) }.join("\n")
-                  end
+        "    no logins found for user."
+      else
+        longest_size = logins.map { |l| l[:location].length }.max
+        logins.map { |l| parse_login(l, longest_size) }.join("\n")
+      end
     <<~HEREDOC
         #{user}:
       #{logins_part}
     HEREDOC
   end
 
+  sig do
+    params(
+      login: T::Hash[T.untyped, T.untyped], # the login data, see {#hashify_login}
+      longest_size: Integer, # the longest string length to help with string formatting
+    ).returns(String) # the formatted string for printing
+  end
   # Takes login data and converts it to a heplful printable string.
-  #
-  # @param login [Hash] the login data, see {#hashify_login}
-  # @param longest_size [Integer] the longest string length to help with string
-  #   formatting
-  #
-  # @return [String] the formatted string for printing
   def parse_login(login, longest_size)
     location = login[:location].ljust(longest_size, " ")
     start = login[:time_start].strftime("%m/%d/%Y %I:%M%p")
     finish = if login[:time_end].is_a? String # not a date
-               if login[:time_end] == "still logged in"
-                 login[:time_end].green
-               else
-                 login[:time_end].yellow
-               end
-             else
-               "#{((login[:time_end] - login[:time_start]) / 60).to_i} minutes"
-             end
+        if login[:time_end] == "still logged in"
+          login[:time_end].green
+        else
+          login[:time_end].yellow
+        end
+      else
+        "#{((login[:time_end] - login[:time_start]) / 60).to_i} minutes"
+      end
     "    from #{location} at #{start} (#{finish})"
   end
 
+  sig { returns(T::Hash[Symbol, T::Hash[String, T::Hash[Symbol, T.untyped]]]) }
   # Takes a list of configured usernames and grabs login data from the system.
-  #
-  # @return [Hash{Symbol => Hash}] a hash with username keys and hash values
-  #   containing the login data (see {#hashify_login})
   def parse_last_logins
     @users.map do |(username, num_logins)|
       cmd_result = `last --time-format=iso #{username}`
@@ -82,24 +84,32 @@ class LastLogin < Component
     end.to_h
   end
 
+  sig do
+    params(
+      login: String, # the raw string result from the call to the system
+      username: String, # the username of the logged user
+    ).returns(T::Hash[Symbol, T.untyped]) # the parsed login entry data, with symbol keys
+  end
   # A hash representation of a single login.
-  #
-  # @param login [String] the raw string result from the call to the system
-  #   containing various bits of login information
-  # @param username [String] the username of the logged user
-  #
-  # @return [Hash] the parsed login entry data, with symbol keys
   def hashify_login(login, username)
     re = login.chomp.split(/(?:\s{2,})|(?<=\d)(?:\s-\s)/)
-    date = re[4].scan(/\d{4}-\d{2}-[\dT:]+-\d{4}/)
 
-    time_end = date.any? ? Time.parse(re[4]) : re[4]
+    if re[2..4].nil? || T.must(re[2..4]).any?(&:nil?)
+      raise "Could not parse output from 'last' command: \n#{login}"
+    end
+
+    location = T.must(re[2])
+    time_start = Time.parse(T.must(re[3]))
+    time_end_raw = T.must(re[4])
+
+    date = time_end_raw.scan(/\d{4}-\d{2}-[\dT:]+-\d{4}/)
+    time_end = date.any? ? Time.parse(time_end_raw) : time_end_raw
 
     {
-      username: username,             # string username
-      location: re[2],                # string login location, an IP address
-      time_start: Time.parse(re[3]),
-      time_end: time_end,             # Time or string, could be "still logged in"
+      username: username,     # string username
+      location: location,     # string login location, an IP address
+      time_start: time_start,
+      time_end: time_end,     # Time or string, could be "still logged in"
     }
   end
 end
