5f50f18df9433253acc78fcf395e667f336bd0f0
Taylor Thurlow <taylorthurlow@me.com>
2020-04-17 01:01:48 -0700

    Add type signatures to Component

--- lib/panda_motd/component.rb
+++ lib/panda_motd/component.rb
@@ -4,6 +4,8 @@
 class Component
   attr_reader :name, :errors, :results, :config
 
+  sig { params(motd: MOTD, name: String).void }
+
   def initialize(motd, name)
     @name = name
     @motd = motd
@@ -11,23 +13,27 @@ class Component
     @errors = []
   end
 
+  sig { void }
   # Evaluates the component so that it has some meaningful output when it comes
   # time to print the MOTD.
   def process
     raise NotImplementedError
   end
 
+  sig { returns(String) }
   # Gives the output of a component as a string.
   def to_s
     raise NotImplementedError
   end
 
+  sig { returns(Integer) }
   # The number of lines to print before the component in the context of the
   # entire MOTD. 1 by default, if not configured.
   def lines_before
     @motd.config.component_config(@name)["lines_before"] || 1
   end
 
+  sig { returns(Integer) }
   # The number of lines to print after the component in the context of the
   # entire MOTD. 1 by default, if not configured.
   def lines_after
