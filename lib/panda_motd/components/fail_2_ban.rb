1c31ccc756bfb7f95d990fc055a8984926331f52
Taylor Thurlow <taylorthurlow@me.com>
2020-04-17 01:14:00 -0700

    Set untyped files to typed: false

--- lib/panda_motd/components/fail_2_ban.rb
+++ lib/panda_motd/components/fail_2_ban.rb
@@ -1,10 +1,14 @@
-# typed: true
+# typed: false
 # frozen_string_literal: true
 
 class Fail2Ban < Component
+  sig { params(motd: MOTD).void }
+
   def initialize(motd)
     super(motd, "fail_2_ban")
   end
+
+  sig { void }
 
   def process
     @results = {
@@ -20,6 +24,8 @@ class Fail2Ban < Component
     end
   end
 
+  sig { returns(String) }
+
   def to_s
     result = +"Fail2Ban:\n"
     @results[:jails].each do |name, stats|
@@ -28,18 +34,31 @@ class Fail2Ban < Component
       result << "    Current bans: #{stats[:current]}\n"
     end
 
-    result.gsub!(/\s$/, "")
+    result.gsub!(/\s$/, "") || ""
   end
 
   private
+
+  sig { params(jail: String).returns(T::Hash[Symbol, Integer]) }
 
   def jail_status(jail)
     cmd_result = `fail2ban-client status #{jail}`
     if cmd_result =~ /Sorry but the jail '#{jail}' does not exist/
       @errors << ComponentError.new(self, "Invalid jail name '#{jail}'.")
     else
-      total = cmd_result.match(/Total banned:\s+([0-9]+)/)[1].to_i
-      current = cmd_result.match(/Currently banned:\s+([0-9]+)/)[1].to_i
+      total_matcher = cmd_result.match(/Total banned:\s+([0-9]+)/)
+      total = if total_matcher
+          total_matcher[1].to_i
+        else
+          raise "Unable to determine number of total bans for jail: #{jail}"
+        end
+
+      current_matcher = cmd_result.match(/Currently banned:\s+([0-9]+)/)
+      current = if current_matcher
+          current_matcher[1].to_i
+        else
+          raise "Unable to determine number of current bans for jail: #{jail}"
+        end
     end
 
     { total: total, current: current }
